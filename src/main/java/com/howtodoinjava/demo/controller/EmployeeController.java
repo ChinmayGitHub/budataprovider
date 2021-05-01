e5f4de86429474af74fe4888f7730c177fe3b099
Chinmay Samant <chinmaysamant@localhost>
2021-05-01 11:37:27 +0530

    fix dc

--- src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -21,7 +21,7 @@ public class EmployeeController {
     public List<Employee> getEmployees() 
     {
 		List<Employee> employeesList = new ArrayList<Employee>();
-		employeesList.add(new Employee(1,"bakla","gupta","howtodoinjava@gmail.com"));
+		employeesList.add(new Employee(1,"bakla","gupdta","howtodoinjava@gmail.com"));
 		return employeesList;
     }
 	
